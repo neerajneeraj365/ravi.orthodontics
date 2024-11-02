fa7605ecdd92799d5cedbcf5423dce3cd1c4977b
Neeraj Neeraj <neerajneeraj@Neerajs-MacBook-Air.local>
2024-11-02 11:32:27 -0400

    added msg

--- app/api/appointment/route.ts
+++ app/api/appointment/route.ts
@@ -1,32 +1,29 @@
 import prisma from "@/lib/prisma";
+import twilio from "twilio";
 
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   try {
+    const accountSid = process.env.accountSid;
+    const authToken = process.env.authToken;
+    const client = require("twilio")(accountSid, authToken);
+
     const { registeringFor, name, phone, age, gender, reason } =
       await req.json();
 
-    console.log(
-      "Received data:",
-      registeringFor,
-      name,
-      phone,
-      age,
-      gender,
-      reason
-    );
-    const booking = await prisma.patient.create({
-      data: {
-        registeringFor,
-        name,
-        phone,
-        age,
-        gender,
-        reason,
-      },
+    const booking = client.messages.create({
+      body: `New appointment booking: 
+      For: ${registeringFor}
+      Name: ${name}
+      Phone: ${phone}
+      Age: ${age}
+      Gender: ${gender}
+      Reason: ${reason}`,
+      from: "+14433907397",
+      to: "+14379705609",
     });
-    return NextResponse.json(booking, { status: 201 });
+    return NextResponse.json(booking, { status: 200 });
   } catch (error) {
     return new NextResponse("Error", {
       status: 500,
